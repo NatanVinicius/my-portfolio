7563f620fe3b163de214a5084c177af7a51dec7d
NatanVinicius <natanviniciusdepaula@gmail.com>
2025-08-10 18:35:41 +1200

    fix workflow-6

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,5 @@
 provider "google" {
-    credentials = file("${path.module}/keys/my-portfolio-468604-d161826bbdb0.json")
-    project = "my-portfolio-468604"
-    region = "us-central1"
-
+  credentials = var.credentials_json
+  project     = var.project_id
+  region      = var.region
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "credentials_json" {
+  type      = string
+  sensitive = true
+}
+
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
