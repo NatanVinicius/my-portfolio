2f8e8ea8cdaabe216b255316e98a1ee9257a81b8
NatanVinicius <natanviniciusdepaula@gmail.com>
2025-08-10 18:30:14 +1200

    fix workflow-5

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-    credentials = file("C:/Terraform/keys/my-portfolio-468604-d161826bbdb0.json")
+    credentials = file("${path.module}/keys/my-portfolio-468604-d161826bbdb0.json")
     project = "my-portfolio-468604"
     region = "us-central1"
 
